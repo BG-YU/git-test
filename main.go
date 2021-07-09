b5cc4e89d9c40e9e23715bc9956f5a38ddfd39df
BG-YU <63723145+BG-YU@users.noreply.github.com>
2021-07-09 16:20:54 +0900

    MODIFY: 수정1 (#1)
    * MODIFY: 수정1
    
    * MODIFY: test2
    
    * MODIFY: test3
    
    Co-authored-by: yu <yu@yuui-MacBookPro.local>

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello, go!")
+	fmt.Println("hello, go!")    // main
+	fmt.Println("feature/test1") // commit1
+	fmt.Println("feature/test1") // commit2
+	fmt.Println("feature/test1") // commit3
 }
